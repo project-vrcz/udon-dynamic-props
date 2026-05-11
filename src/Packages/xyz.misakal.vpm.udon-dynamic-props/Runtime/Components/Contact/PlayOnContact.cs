37afa5153475d5c6c2cb62d28a201c76c9ca88e7
Misaka_L <lipww1234@foxmail.com>
2026-05-11 23:45:22 +0800

    feat: play on contact component

---
+++ src/Packages/xyz.misakal.vpm.udon-dynamic-props/Runtime/Components/Contact/PlayOnContact.cs
@@ -0,0 +1,45 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Dynamics;
+
+namespace UdonDynamicProps.Runtime.Components.Contact
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public sealed class PlayOnContact : UdonSharpBehaviour
+    {
+        public AudioSource audioSource;
+        public AudioClip audioClip;
+
+        public bool playOnEnter;
+        public bool playOnExit;
+
+        public override void OnContactEnter(ContactEnterInfo contactInfo)
+        {
+            if (!playOnEnter) return;
+            PlayOneShot();
+        }
+
+        public override void OnContactExit(ContactExitInfo contactInfo)
+        {
+            if (!playOnExit) return;
+            PlayOneShot();
+        }
+
+        private void PlayOneShot()
+        {
+            if (!audioSource)
+            {
+                Debug.LogWarning("Please assign AudioSource to PlayOnContactEnter component.", this);
+                return;
+            }
+
+            if (!audioClip)
+            {
+                Debug.LogWarning("Please assign AudioClip to PlayOnContactEnter component.", this);
+                return;
+            }
+
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+}
